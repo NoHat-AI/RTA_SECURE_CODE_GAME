207a4fc807785a8147611b846505aad86deb0d4a
NoHat-AI <79945842+NoHat-AI@users.noreply.github.com>
2024-06-04 07:08:30 +0000

    test

--- Season-1/Level-4/code.py
+++ Season-1/Level-4/code.py
@@ -200,10 +200,10 @@ class DB_CRUD_ops(object):
 
             res = "[METHOD EXECUTED] update_stock_price\n"
             # UPDATE stocks SET price = 310.0 WHERE symbol = 'MSFT'
-            query = "UPDATE stocks SET price = '%d' WHERE symbol = '%s'" % (price, stock_symbol)
+            query = "UPDATE stocks SET price = '%d' WHERE symbol = '%s'" % (sanitized_price, sanitized_stock_symbol)
             res += "[QUERY] " + query + "\n"
 
-            cur = db_con.execute(query, (sanitized_stock_symbol,price,))
+            cur = db_con.execute(query, (price, sanitized_stock_symbol,))
             db_con.commit()
             query_outcome = cur.fetchall()
             for result in query_outcome:
